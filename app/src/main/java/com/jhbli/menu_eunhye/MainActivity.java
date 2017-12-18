da53a1912b0ae9b538503e659743a2c98dd28c19
eunhye1119 <jhbling0408@gmail.com>
2017-12-18 21:12:41 +0900

    -a

--- app/src/main/java/com/jhbli/menu_eunhye/MainActivity.java
+++ app/src/main/java/com/jhbli/menu_eunhye/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jhbli.menu_eunhye;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jhbli.menu_eunhye.fragments.FragmentAttend;
 import com.jhbli.menu_eunhye.fragments.FragmentBus;
@@ -17,9 +20,16 @@ import com.jhbli.menu_eunhye.fragments.FragmentCall;
 import com.jhbli.menu_eunhye.fragments.FragmentNotice;
 import com.jhbli.menu_eunhye.fragments.FragmentSettings;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {//implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
+    private static final int MSG_TIMER_EXPIRED = 1;
+    private static final int BACKKEY_TIMEOUT = 2;
+    private static final int MILLIS_IN_SEC = 1000;
+    private long mCurrTimeInMillis = 0;
+    private boolean mIsBackKeyPressed = false;
     static String[] menus = {"등원관리","셔틀버스 조회","센터연결","설정","공지사항"};
 
     @Override
@@ -123,13 +133,46 @@ public class MainActivity extends AppCompatActivity {//implements NavigationView
 
     @Override
     public void onBackPressed() {
+/*
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
+        */
+        if (mIsBackKeyPressed == false){
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+            else {
+                mIsBackKeyPressed = true; // 한번 누른 상태로 변경
+                mCurrTimeInMillis = Calendar.getInstance().getTimeInMillis();
+                Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+                startTimer();
+            }
+        }
+        else {
+            mIsBackKeyPressed = false; // 백키가 한번 눌린상태면
+            if (Calendar.getInstance().getTimeInMillis() <= (mCurrTimeInMillis + (BACKKEY_TIMEOUT * MILLIS_IN_SEC))){
+                finish();
+            }
+        }
     }
+    private void startTimer(){
+        mTimerHandler.sendEmptyMessageDelayed(MSG_TIMER_EXPIRED, BACKKEY_TIMEOUT * MILLIS_IN_SEC);
+    }
+    private Handler mTimerHandler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case MSG_TIMER_EXPIRED:
+                    mIsBackKeyPressed = false;
+                break;
+            }
+        }
+    };
+
+
 
 /*
     @SuppressWarnings("StatementWithEmptyBody")
