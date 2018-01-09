874aa42bbcb59e2d0ddad66196ebfe711c26512e
eunhye1119 <hyeya180@gmail.com>
2018-01-10 00:53:52 +0900

    .

---
+++ app/src/main/java/com/jhbli/menu_eunhye/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.jhbli.menu_eunhye.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jhbli.menu_eunhye.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
