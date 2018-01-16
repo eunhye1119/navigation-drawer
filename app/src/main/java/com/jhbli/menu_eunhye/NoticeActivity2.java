792be7902b66d08c64f723a1986e0700fe115a26
eunhye1119 <hyeya180@gmail.com>
2018-01-16 14:17:08 +0900

    .

---
+++ app/src/main/java/com/jhbli/menu_eunhye/NoticeActivity2.java
@@ -0,0 +1,13 @@
+package com.jhbli.menu_eunhye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice2);
+    }
+}
